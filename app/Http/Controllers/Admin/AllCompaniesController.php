f356ed462edaef6764a245cd573e70513d4051d5
sabreenMohammed107 <sabreenm312@gmail.com>
2020-10-10 21:24:43 +0200

    company updates

--- app/Http/Controllers/Admin/AllCompaniesController.php
+++ app/Http/Controllers/Admin/AllCompaniesController.php
@@ -41,7 +41,7 @@ class AllCompaniesController extends Controller
     public function index()
     {
         $rows = Company::where('active', 1)->where('id','!=',100)->paginate(8);
-       
+       return($rows);
          return view($this->viewName . 'index', compact('rows'));
     }
 
