d6d0f87951298509825248c068f4bb8b49f00a8d
sabreenMohammed107 <sabreenm312@gmail.com>
2020-10-17 18:52:06 +0200

    editing

--- app/Http/Controllers/Admin/Reports/SuppliersReportController.php
+++ app/Http/Controllers/Admin/Reports/SuppliersReportController.php
@@ -42,7 +42,7 @@ class SuppliersReportController extends Controller
       
         $rows = Company::whereIn('id', $exception)->where('id', '!=', 100)->get();
         if(Auth::user()->role_id == 110){
-            $companies = Company::where('id', '!=', 100)->orderBy("created_at", "Desc")->get();
+            $rows = Company::where('id', '!=', 100)->orderBy("created_at", "Desc")->get();
         }
         // $rows = Company::where('active', 1)->where('id', '!=', 100)->get();
         $suppliers = [];
