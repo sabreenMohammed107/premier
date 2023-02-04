0c04fa24c7c7062ed558f11aa900c0a6976e98c3
sabreenMohammed107 <sabreenm312@gmail.com>
2023-02-04 21:19:19 +0200

    test 4-2-2023

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
+     
        
             Alkoumi\LaravelArabicTafqeet\LaravelArabicTafqeetServiceProvider::class,
             Mccarlosen\LaravelMpdf\LaravelMpdfServiceProvider::class
@@ -234,7 +234,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        
         'PDF' => Mccarlosen\LaravelMpdf\Facades\LaravelMpdf::class
 
     ],
